93bd2c1a220e14995eac7e7a04eb1a3c3aef5388
dmytro.kotliarchuk <dmytro.kotliarchuk@gmail.com>
2022-12-24 19:53:37 -0800

    create Network and visualize process

--- car.js
+++ car.js
@@ -11,10 +11,16 @@ class Car {
         this.friction = 0.05;
         this.angle = 0;
 
+        this.useBrain = controlType === "AI";
+
         this.damaged = false;
 
-        if (controlType === "KEYS") {
+        if (controlType === "KEYS" || controlType === "AI") {
             this.sensor = new Sensor(this);
+            this.brain = new NeuralNetwork(
+                [this.sensor.rayCount, 6, 4]
+            );
+
         }
 
         this.controls = new Controls(controlType);
@@ -29,6 +35,19 @@ class Car {
 
         if (this.sensor) {
             this.sensor.update(roadBoarders, traffic);
+            const offsets = this.sensor.readings.map(
+                s => s == null ? 0 : 1 - s.offset
+            );
+
+            const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+            console.log(outputs);
+
+            if (this.useBrain) {
+                this.controls.forward = outputs[0];
+                this.controls.left = outputs[1];
+                this.controls.right = outputs[2];
+                this.controls.reverse = outputs[3];
+            }
         }
     }
 
